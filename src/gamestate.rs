3dec0fa1807cc682b97e01a53fdf247abd734d9f
joomakelastudent <joonas.makela@student.lut.fi>
2023-02-04 11:54:02 +0200

    added new data to structs

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -3,22 +3,27 @@ struct GameState {
     player_state : PlayerState,
 }
 
+struct MapState {
+    data: String,
+}
+
 struct PlayerState {
-    location : Location,
-    health : Health,
-    status : 
+    location: Location,
+    health: Health,
+    dash_available: bool,
+    dash_cooldown: i32,
 }
 
 struct Location {
-    x : i32,
-    y : i32,
+    x: i32,
+    y: i32,
 }
 
 struct EnemyState {
-    behavior : EnemyBehavior,
-    health : Health,
-    species : EnemySpecies,
-    location : Location,
+    behavior: EnemyBehavior,
+    health: Health,
+    species: EnemySpecies,
+    location: Location,
 }
 
 enum EnemyBehavior {
