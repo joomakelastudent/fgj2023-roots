f82f41f08287b37b05e157eb1dfb723f3c5c69fe
joomakelastudent <joonas.makela@student.lut.fi>
2023-02-04 11:59:10 +0200

    Added file header

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Finnish Game Jam 2023-02-03 project of team Ryhmä Rämä
+ * Project name: tty-throwdown
+ * Authors: see the AUTHORS.md file
+ * License: Apache 2.0 license
+ * Repo: https://github.com/joomakelastudent/fgj2023-roots
+ ******************************************************************************/
+
 struct GameState {
     map_state : MapState,
     player_state : PlayerState,
