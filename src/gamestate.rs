98ea36150887731c5965de9f9ada275e7e1f7bb5
joomakelastudent <joonas.makela@student.lut.fi>
2023-02-04 11:21:56 +0200

    docs push

---
+++ src/gamestate.rs
@@ -0,0 +1,27 @@
+struct GameState {
+    map_state : MapState,
+    player_state : PlayerState,
+}
+
+struct PlayerState {
+    location : Location,
+    health : Health,
+}
+
+struct Location {
+    x : i32,
+    y : i32,
+}
+
+struct EnemyState {
+    behavior : EnemyBehavior,
+    health : Health,
+    species : EnemySpecies,
+    location : Location,
+}
+
+enum EnemyBehavior {
+    IDLE,
+    MOVE_TO (Location),
+    ATTACK,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,62 @@
+/*******************************************************************************
+ * Finnish Game Jam 2023-02-03 project of team Ryhmä Rämä
+ * Project name: tty-throwdown
+ * Authors: see the AUTHORS.md file
+ * License: Apache 2.0 license
+ * Repo: https://github.com/joomakelastudent/fgj2023-roots
+ ******************************************************************************/
+
+// Standard library imports
+use std::collections::HashMap;
+use std::io;
+
+// Crate imports
+use tui;
+
+// Local file imports
+mod render;
+
+// Public constant declarations
+
 /*
 should level be mutable or not?
 probably yes. it's easier to handle one state than to handle one state and then the exceptions.
 */
 
-
-// main game loop
+/* Where the preliminary setup for the game happens
+ * Mostly does one-time work that the main loop requires to start
+ * Locates and "loads" the level from the filesystem
+ * Verifys that the terminal window has enough rows and columns
+ * Starts up the main game loop
+ * Takes no parameters
+ * Returns an exit status to the OS
+ */
 fn main() {
-    level = 
+    let level = "file_path_here";
+
+    // If we get here, assume everything is good to go
     gameloop(level);
 }
 
 fn load_level(address) {
-    
+
 }
 
+/* Where the main logic of the game happens
+ * Organizes all of the separate systems of the game
+ * Advances all systems by one step each cycle
+ * One cycle equals one "tick" of the game
+ * Takes the level data as a parameter
+ * Returns nothing
+ */
 fn gameloop() {
-    do {
-    println!("Hello, world!");
+    // We need some basic info from the state to start/advance the main loop
+    initialize_game_state(&mut game_state);
 
-    //input_state = jotain()
-    //
+    loop {
+        // Take input from the player
+        // let input_state = get_input_state();
 
-    //update game_state either OOP or fp. whatever makes sense.
-    //OOP is faster and better at small scale
-
-    //render(game_state)
-    } while (true);
+        // 
+    }
 }
