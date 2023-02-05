b730a520f52d4245523da3dde1e4303cd89c43a9
joomakelastudent <joonas.makela@student.lut.fi>
2023-02-05 15:01:35 +0200

    updated header files

--- src/render.rs
+++ src/render.rs
@@ -2,7 +2,7 @@
  * Finnish Game Jam 2023-02-03 project of team Ryhmä Rämä
  * Project name: tty-throwdown
  * Authors: see the AUTHORS.md file
- * License: Apache 2.0 license
+ * License: Attribution-Noncommercial-Share Alike 3.0 license
  * Repo: https://github.com/joomakelastudent/fgj2023-roots
  ******************************************************************************/
 
